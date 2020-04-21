4d873691d87a975ea92acfed413b7ed8c7e34c61
El_mitya <el_mitya@mail.ru>
2020-04-21 18:03:32 +0300

    commit2

---
+++ lab/Apartment.py
@@ -0,0 +1,41 @@
+class Room():
+
+    def __init__(self, area_room, people, area_room_additionally):
+        self.area_room = area_room
+        self.people = people
+        self.area_room_additionally = area_room_additionally
+
+
+    def get_age(self):
+        return self.area_room,
+        return self.people
+        return self.area_room_additionally
+
+        # setter method
+
+    def set_age(self, x, y, c):
+        self._area_room = x
+        self.people = y
+        self.area_room_additionally = c
+
+
+class Apartment(Room):
+    area_room1 = float(input())
+    people1 = int(input())
+    apartments = Room(area_room1, people1)
+
+    def area_coming_per_one_person(self):
+        pass
+
+    def average_area_per_person_taking_into_account(self):
+        pass
+
+    def total_area(self):
+        pass
+
+    def minimum_area_per_person(self):
+        pass
+
+
+
+
